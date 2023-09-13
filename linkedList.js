1d76ad7a9799d595cc3dcfe2d7b3e12c2b32fccc
RobVerplanke <rverplanke@gmail.com>
2023-09-13 15:26:10 +0200

    set up classes and methods

---
+++ linkedList.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(value){
+        this.head = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value){
+    }
+
+    prepend(value){
+    }
+
+    size(){
+    }
+
+    head(){
+    }
+
+    tail(){
+    }
+
+    at(index){
+    }
+
+    pop(){
+    }
+    
+    contains(value){
+    }
+
+    find(value){
+    }
+
+    toString(){
+    }
+
+    insertAt(value, index){
+    }
+
+    removeAt(index){
+    }
+
+}
+
+
