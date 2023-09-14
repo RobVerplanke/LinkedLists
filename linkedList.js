115fe9dd5266636076befd06720729d9c6779db4
RobVerplanke <rverplanke@gmail.com>
2023-09-14 17:20:24 +0200

    create pop function wich deletes the last node in the list and create the print function

--- linkedList.js
+++ linkedList.js
@@ -14,7 +14,9 @@ class LinkedList{
     }
 
     append(value){
-        if(this.tail === null){
+
+        // If the list is empty, create new node and refer the head and tail to it
+        if(!this.tail){
             const newNode = new Node(value);
 
             this.head = newNode;
@@ -24,6 +26,7 @@ class LinkedList{
 
         } else{
 
+            // IF the list is not empty, create new node. Set new node as last node
             const storedNode = this.tail;
             const newNode = new Node(value);
 
@@ -55,8 +58,26 @@ class LinkedList{
     }
 
     pop(){
+
+        // If list is empty, do nothing
+        if(this.tail == null) return;
+        
+        // If list has one node, empty the head and tail
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+            this.length = 0;
+            return;
+        }
+      
+        // If there are more than one nodes in the list, remove the 'next' reference 
+        // and set the previous node as 'tail
+        let newTail = this.tail.prev
+        newTail.next = null;
+        this.tail = newTail;
+        this.length--;
     }
-    
+
     contains(value){
     }
 
@@ -64,6 +85,14 @@ class LinkedList{
     }
 
     toString(){
+        let current = this.head;
+
+        // If there is a 'head' node, print the value and do the same for the next node
+        // until there are no more node left
+        while(current !== null){
+            console.log(current.value);
+            current = current.next;
+        }
     }
 
     insertAt(value, index){
@@ -81,6 +110,10 @@ newList.append(20);
 newList.append(30);
 newList.append(40);
 
+console.log(`length: ${newList.length}, list:`);
+newList.toString();
 
+newList.pop();
+console.log(`length: ${newList.length}, new list:`);
 
-
+newList.toString();
